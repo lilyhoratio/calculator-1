04587e12b8761f5edc137b1172f12a793e99fe93
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 12:16:27 -0700

    Adding new arithmetic and caluclator python files

---
+++ arithmetic.py
@@ -0,0 +1,50 @@
+"""Math functions for calculator."""
+
+
+def add(num1, num2):
+    """Return the sum of the two inputs."""
+    
+    add_sum = num1 + num2
+    return add_sum
+
+def subtract(num1, num2):
+    """Return the second number subtracted from the first."""
+    
+    diff = num1 - num2
+    return diff
+
+def multiply(num1, num2):
+    """Multiply the two inputs together."""
+    
+    product = num1 * num2
+    return product
+
+def divide(num1, num2):
+    """Divide the first input by the second and return the result."""
+    
+    quotient = num1 / num2
+    return quotient
+
+def square(num1):
+    """Return the square of the input."""
+    
+    my_square = num1 ** 2
+    return my_square
+
+def cube(num1):
+    """Return the cube of the input."""
+    
+    my_cube = num1 ** 3
+    return my_cube
+
+def power(num1, num2):
+    """Raise num1 to the power of num2 and return the value."""
+    
+    my_power = num1 ** num2
+    return my_power
+
+def mod(num1, num2):
+    """Return the remainder of num1 / num2."""
+
+    my_mod = num1 % num2
+    return my_mod
